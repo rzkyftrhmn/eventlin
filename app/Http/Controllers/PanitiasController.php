abaf626d7957a05408f62c82a13fd5f12776a2f1
raditrizq <radityaanwar511@gmaile.com>
2025-07-12 11:16:16 +0700

    edit untuk menambah password di panitia dan peserta

--- app/Http/Controllers/PanitiasController.php
+++ app/Http/Controllers/PanitiasController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Divisi;
 use App\Models\Panitia;
 use App\Models\Proposal;
+use Illuminate\Validation\Rule as ValidationRule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PanitiasController extends Controller
 {
@@ -24,19 +26,31 @@ class PanitiasController extends Controller
     
     public function store(Request $request, $id_proposal)
     {
-        $request->validate([
+            $request->validate([
             'nama_panitia' => 'required|string|max:255',
+            'email' => 'required|email|unique:panitia,email',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/[a-z]/',        // Huruf kecil
+                'regex:/[A-Z]/',        // Huruf besar
+                'regex:/[0-9]/',        // Angka
+                'confirmed'             
+            ],
             'jabatan_panitia' => 'required|in:Ketua,Sekretaris,Bendahara,Panitia',
             'id_divisi' => 'nullable|required_if:jabatan_panitia,Panitia',
         ]);
-    
+
         Panitia::create([
             'id_proposal' => $id_proposal,
             'id_divisi' => $request->jabatan_panitia === 'Panitia' ? $request->id_divisi : null,
             'nama_panitia' => $request->nama_panitia,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
             'jabatan_panitia' => $request->jabatan_panitia,
         ]);
-    
+
         return redirect()->route('panitia.index', $id_proposal)->with('success', 'Panitia berhasil ditambahkan.');
     }
     
@@ -48,23 +62,40 @@ class PanitiasController extends Controller
     }
     
     public function update(Request $request, $id_panitia)
-    {
-        $panitia = Panitia::findOrFail($id_panitia);
-    
+   {
+       $panitia = Panitia::findOrFail($id_panitia);
+
         $request->validate([
             'nama_panitia' => 'required|string|max:255',
+            'email' => ['required','email',ValidationRule::unique('panitia', 'email')->ignore($panitia->id_panitia, 'id_panitia')],
+            'password' =>[
+                'nullable',
+                'string',
+                'min:8',
+                'regex:/[a-z]/',        // Huruf kecil
+                'regex:/[A-Z]/',        // Huruf besar
+                'regex:/[0-9]/',        // Angka
+         
+            ],
             'jabatan_panitia' => 'required|in:Ketua,Sekretaris,Bendahara,Panitia',
             'id_divisi' => 'nullable|required_if:jabatan_panitia,Panitia',
         ]);
-    
-        $panitia->update([
+
+        $data = [
             'nama_panitia' => $request->nama_panitia,
+            'email' => $request->email,
             'jabatan_panitia' => $request->jabatan_panitia,
             'id_divisi' => $request->jabatan_panitia === 'Panitia' ? $request->id_divisi : null,
-        ]);
-    
+        ];
+
+        if ($request->filled('password')) {
+            $data['password'] = Hash::make($request->password);
+        }
+
+        $panitia->update($data);
+
         return redirect()->route('panitia.index', $panitia->id_proposal)->with('success', 'Data panitia berhasil diperbarui.');
-    }
+   }
     
     public function destroy($id_panitia)
     {
--- resources/views/panitia/form.blade.php
+++ resources/views/panitia/form.blade.php
@@ -1,9 +1,43 @@
 @csrf
+
+{{-- Nama --}}
 <div>
     <label>Nama Panitia:</label>
     <input type="text" name="nama_panitia" value="{{ old('nama_panitia', $panitia->nama_panitia ?? '') }}" required>
+    @error('nama_panitia')
+        <div style="color: red;">{{ $message }}</div>
+    @enderror
 </div>
 
+{{-- Email --}}
+<div>
+    <label>Email:</label>
+    <input type="email" name="email" value="{{ old('email', $panitia->email ?? '') }}" required>
+    @error('email')
+        <div style="color: red;">{{ $message }}</div>
+    @enderror
+</div>
+
+{{-- Password --}}
+<div>
+    <label>Password:</label>
+    <input type="password" name="password"
+        @if(isset($panitia)) placeholder="Biarkan kosong jika tidak ingin mengubah password" @endif
+        {{ isset($panitia) ? '' : 'required' }}>
+    @error('password')
+        <div style="color: red;">{{ $message }}</div>
+    @enderror
+</div>
+
+{{-- Konfirmasi Password --}}
+<div>
+    <label>Konfirmasi Password:</label>
+    <input type="password" name="password_confirmation"
+        @if(isset($panitia)) placeholder="Ulangi password jika ingin mengganti" @endif
+        {{ isset($panitia) ? '' : 'required' }}>
+</div>
+
+{{-- Jabatan --}}
 <div>
     <label>Jabatan:</label>
     <select name="jabatan_panitia" id="jabatan" required>
@@ -11,8 +45,12 @@
             <option value="{{ $jabatan }}" {{ (old('jabatan_panitia', $panitia->jabatan_panitia ?? '') == $jabatan) ? 'selected' : '' }}>{{ $jabatan }}</option>
         @endforeach
     </select>
+    @error('jabatan_panitia')
+        <div style="color: red;">{{ $message }}</div>
+    @enderror
 </div>
 
+{{-- Divisi (Kondisional) --}}
 <div id="divisi-container" style="display: none;">
     <label>Divisi:</label>
     <select name="id_divisi">
@@ -23,8 +61,12 @@
             </option>
         @endforeach
     </select>
+    @error('id_divisi')
+        <div style="color: red;">{{ $message }}</div>
+    @enderror
 </div>
 
+{{-- Script untuk toggle divisi --}}
 <script>
 document.addEventListener('DOMContentLoaded', function () {
     const jabatanSelect = document.getElementById('jabatan');
